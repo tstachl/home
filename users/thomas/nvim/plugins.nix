148170f6682be16518b43f396004236c7b20ea26
Thomas Stachl <i@t5.st>
2025-01-24 16:20:06 -0500

    forgot some things for my editor

--- users/thomas/nvim/plugins.nix
+++ users/thomas/nvim/plugins.nix
@@ -1,12 +1,19 @@
 {
   imports = [
-    ./plugins/cmp.nix
+    ./lsp.nix
+    ./plugins/avante.nix
+    ./plugins/conform.nix
     ./plugins/comment.nix
     ./plugins/fidget.nix
+    ./plugins/gitsigns.nix
     ./plugins/indent-blankline.nix
+    ./plugins/lint.nix
     ./plugins/mdx.nix
+    ./plugins/mini.nix
+    ./plugins/oil.nix
     ./plugins/telescope.nix
     ./plugins/tmux.nix
+    ./plugins/todo-comments.nix
     ./plugins/treesitter.nix
     ./plugins/web-devicons.nix
     ./plugins/which-key.nix
