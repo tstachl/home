df4900a44ec8ee2138e7c97a529e6a61b8eb2117
Thomas Stachl <i@t5.st>
2025-02-10 20:00:57 +0100

    choose does not ask to overwrite

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -5,15 +5,14 @@
     shellAliases = import ./shell-aliases.nix;
 
     interactiveShellInit = lib.mkAfter ''
-      ${lib.strings.fileContents (
-        pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "fish";
-          rev = "cc8e4d8";
-          sha256 = "sha256-udiU2TOh0lYL7K7ylbt+BGlSDgCjMpy75vQ98C1kFcc=";
-        }
-        + "/themes/Catppuccin Frappe.theme"
-      )}
+      fish_config theme choose "Catppuccin Frappe"
     '';
   };
+
+  xdg.configFile."fish/themes/Catppuccin Frappe.theme".text = lib.strings.fileContents (pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "fish";
+    rev = "cc8e4d8";
+    sha256 = "sha256-udiU2TOh0lYL7K7ylbt+BGlSDgCjMpy75vQ98C1kFcc=";
+  } + "/themes/Catppuccin Frappe.theme");
 }
