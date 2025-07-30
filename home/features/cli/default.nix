f65ceee05e94b420c52e70e245ffb9e42382af70
Thomas Stachl <i@t5.st>
2025-07-30 23:01:32 +0200

    feature: added `fzf` and remapped `cd` => `z` and `grep` => `rg`

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -14,6 +14,7 @@
 
   programs.bash.enable = true;
   programs.direnv.enable = true;
+  programs.fzf.enable = true;
   programs.zoxide.enable = true;
   programs.zsh.enable = true;
 }
--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -1,6 +1,10 @@
 { lib, pkgs, ... }:
 {
   programs.fish.enable = true;
+  programs.fish.shellAliases = {
+    cd = "z";
+    grep = "rg";
+  };
 
   xdg.configFile."fish/conf.d/kanagawa.fish".text =
     lib.strings.fileContents (pkgs.fetchFromGitHub
